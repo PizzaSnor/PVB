d875110699e394b1c1cfaa098bb62e043237ab59
IgeBruin <iabbruin@st.noorderpoort.nl>
2024-05-09 15:06:41 +0200

    Squashed commit of the following:
    commit 69a5845e7157e11ba474bbf3de663e137811b901
    Author: IgeBruin <iabbruin@st.noorderpoort.nl>
    Date:   Thu May 9 15:02:01 2024 +0200
    
        Adjustable logo size
    
    commit 382988e40ae52862ace4e0778eb98b02a57e9687
    Author: IgeBruin <iabbruin@st.noorderpoort.nl>
    Date:   Thu May 9 14:59:06 2024 +0200
    
        navbar progress
    
    commit c78dfdf4e92c8301469c9b67145c7e353f68bb22
    Author: IgeBruin <iabbruin@st.noorderpoort.nl>
    Date:   Thu May 9 14:23:29 2024 +0200
    
        Tailwind config

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
+// ->middleware(['auth', 'verified'])
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

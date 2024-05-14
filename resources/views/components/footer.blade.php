62dbe0818042a1da2abb430d015b47a2fb791cf0
IgeBruin <iabbruin@st.noorderpoort.nl>
2024-05-14 13:11:51 +0200

    Update Footer contact button

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="flex flex-col gap-y-4 justify-center bg-black mt-10 p-6">
     <h1 class="text-white font-bold text-4xl">NP-Auto</h1>
     <p class="text-white font-semibold text-2xl md:w-2/5">Heb je vragen? Neem contact met ons op! Of ga naar de contact pagina</p>
-    <x-yellow-button content="Contact"/>
-</footer>
+    <a href="{{route('contact')}}">
+        <x-yellow-button content="Contact"/>
+    </a></footer>
